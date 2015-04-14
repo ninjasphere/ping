86358f643d1484fa5b493bc6c6ec0e6792030dcf
☃ Elliot Shepherd <elliot@ninjablocks.com>
2015-04-14 15:46:35 +1000

    Basic working

--- index.js
+++ index.js
@@ -1,8 +1,58 @@
-var http = require('http');
+'use strict';
 
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+var forwarded = require('forwarded-for');
+var express = require('express');
+var app = express();
+
+var seen = {};
+
+var ONE_HOUR = 1000 * 60 * 60;
+
+setInterval(function() {
+
+  for (var publicIP in seen) {
+    for (var localIP in seen[publicIP]) {
+      if ((new Date().getTime() - seen[publicIP][localIP].time) > ONE_HOUR) {
+        delete seen[publicIP][localIP];
+      }
+    }
+  }
+
+}, ONE_HOUR / 4);
+
+
+app.get('/', function (req, res) {
+
+  var publicIP = forwarded(req, req.headers).ip;
+  seen[publicIP] = seen[publicIP] || {};
+
+  var localIP = req.query.ip;
+
+  if (localIP) {
+    seen[publicIP][localIP] = {
+        time: new Date().getTime(),
+        ip: localIP,
+        id: req.query.id
+    }
+  }
+
+  var ips = [];
+  for (var ip in seen[publicIP]) {
+    ips.push({
+      age: new Date().getTime() - seen[publicIP][ip].time,
+      id: seen[publicIP][ip].id,
+      ip: seen[publicIP][ip].ip
+    });
+  }
+
+  res.send(ips);
 });
 
-server.listen((process.env.PORT || 5000));
+var server = app.listen((process.env.PORT || 8888), function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
